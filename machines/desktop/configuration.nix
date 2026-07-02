f531901d35d1ec9b19abec71d239d9e4cd74e0e7
larkin1 <88820972+larkin1@users.noreply.github.com>
2026-07-02 15:20:24 +1000

    reduce number of configurations kept on the machine (i ran out of storage in /boot)

--- machines/desktop/configuration.nix
+++ machines/desktop/configuration.nix
@@ -13,6 +13,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 3;
 
   boot.tmp.useTmpfs = true;
 
