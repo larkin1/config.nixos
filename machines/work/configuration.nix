6284e0719d0ec1d2dc717f912ac3ade19ffb16af
larkin1 <88820972+larkin1@users.noreply.github.com>
2026-07-08 16:04:19 +1000

    orcaslicer idk it still doesn't work im gonna kms

--- machines/work/configuration.nix
+++ machines/work/configuration.nix
@@ -26,6 +26,8 @@
       iptables -I INPUT -m pkttype --pkt-type multicast -j ACCEPT
       iptables -A INPUT -m pkttype --pkt-type multicast -j ACCEPT
       iptables -I INPUT -p udp -m udp --match multiport --dports 1990,2021 -j ACCEPT
+      iptables -I INPUT -p tcp -m tcp --dport 990 -j ACCEPT
+      iptables -I INPUT -p tcp -m tcp --dport 8883 -j ACCEPT
     '';
   };
 
