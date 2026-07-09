e20b865fe0421de0b987694a812dca256b280ebc
larkin1 <88820972+larkin1@users.noreply.github.com>
2026-07-09 17:17:51 +1000

    use a less sketchy way of linking the hypr per-host files

--- lib/user/hypr.nix
+++ lib/user/hypr.nix
@@ -2,13 +2,14 @@
  
 {
   hjem.users."${username}".files = {
-    ".config/hypr".source = pkgs.runCommand "hypr-config" {} ''
-      mkdir -p $out
-      cp -r ${../../configs/hypr}/. $out/
-      chmod -R u+w $out
-      cp    ${hostDir + "/home/monitors.lua"}  $out/lua/monitors.lua
-      cp    ${hostDir + "/home/devices.lua"}   $out/lua/devices.lua
-    '';
+    ".config/hypr".source = pkgs.symlinkJoin {
+      name = "hypr-config";
+      paths = [
+        ../../configs/hypr
+        (pkgs.writeTextDir "lua/monitors.lua" (builtins.readFile "${hostDir + "/home/monitors.lua"}"))
+        (pkgs.writeTextDir "lua/devices.lua" (builtins.readFile "${hostDir + "/home/devices.lua"}"))
+      ];
+    };
 
     ".config/wallpapers".source = "${inputs.config-walls}";
   };
