2311d69b66c0ce2dab9065f85851a3e02000e4e2
larkin1 <88820972+larkin1@users.noreply.github.com>
2026-07-16 18:23:31 +1000

    switch to limine

--- lib/system/core/bootloader.nix
+++ lib/system/core/bootloader.nix
@@ -9,34 +9,35 @@
       "quiet"
       "rd.udev.log_level=3"
       "rd.systemd.show_status=auto"
-      "logo.nologo"
     ];
 
     loader = {
       efi.canTouchEfiVariables = true;
-      timeout = 0;
-      grub = {
-        enable = true;
-        device = "nodev";
-        efiSupport = true;
-        theme = ../../../configs/grub;
-        backgroundColor = "#1E1E2E";
-        gfxmodeEfi = "auto";
-      };
-      # limine = {
+      timeout = 5;
+
+      # grub = {
       #   enable = true;
-      #   extraConfig = ''
-      #     term_palette: 1e1e2e;f38ba8;a6e3a1;f9e2af;89b4fa;f5c2e7;94e2d5;cdd6f4
-      #     term_palette_bright: 585b70;f38ba8;a6e3a1;f9e2af;89b4fa;f5c2e7;94e2d5;cdd6f4
-      #     term_background: 1e1e2e
-      #     term_foreground: cdd6f4
-      #     term_background_bright: 585b70
-      #     term_foreground_bright: cdd6f4
-      #     interface_branding_color: b4befe
-      #     interface_help_color: b4befe
-      #     interface_help_color_bright: b4befe
-      #   '';
+      #   device = "nodev";
+      #   efiSupport = true;
+      #   theme = ../../../configs/grub;
+      #   backgroundColor = "#1E1E2E";
+      #   gfxmodeEfi = "auto";
       # };
+
+      limine = {
+        enable = true;
+        extraConfig = ''
+          term_palette: 1e1e2e;f38ba8;a6e3a1;f9e2af;89b4fa;f5c2e7;94e2d5;cdd6f4
+          term_palette_bright: 585b70;f38ba8;a6e3a1;f9e2af;89b4fa;f5c2e7;94e2d5;cdd6f4
+          term_background: 1e1e2e
+          term_foreground: cdd6f4
+          term_background_bright: 585b70
+          term_foreground_bright: cdd6f4
+          interface_branding_color: b4befe
+          interface_help_color: b4befe
+          interface_help_color_bright: b4befe
+        '';
+      };
     };
   };
 }
