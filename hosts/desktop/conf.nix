94268286cdaa0591e8ba86e2517c434abfbde80b
larkin1 <88820972+larkin1@users.noreply.github.com>
2026-08-05 20:57:16 +1000

    new mouse config

--- hosts/desktop/conf.nix
+++ hosts/desktop/conf.nix
@@ -1,27 +1,33 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  imports =
-    [
-      ./hardware.nix
-      ./home.nix
-      ../../lib/system/core/locale.nix
-      ../../lib/system/core/bootloader.nix
-      ../../lib/system/core/base.nix
+  imports = [
+    ./hardware.nix
+    ./home.nix
+    ../../lib/system/core/locale.nix
+    ../../lib/system/core/bootloader.nix
+    ../../lib/system/core/base.nix
 
-      ../../lib/system/hardware/disk-optimisations.nix
-      ../../lib/system/hardware/nvidia.nix
+    ../../lib/system/hardware/disk-optimisations.nix
+    ../../lib/system/hardware/nvidia.nix
 
-      ../../lib/system/services/bluetooth.nix
-      ../../lib/system/services/network.nix
-      ../../lib/system/services/pipewire.nix
-      ../../lib/system/services/nordvpn.nix
+    ../../lib/system/services/bluetooth.nix
+    ../../lib/system/services/network.nix
+    ../../lib/system/services/pipewire.nix
+    ../../lib/system/services/nordvpn.nix
 
-      ../../lib/system/desktop/fonts.nix
-      ../../lib/system/desktop/desktop.nix
+    ../../lib/system/desktop/fonts.nix
+    ../../lib/system/desktop/desktop.nix
 
-      ../../lib/system/dev/nvim.nix
-    ];
+    ../../lib/system/dev/nvim.nix
+  ];
+
+  services.udev.packages = [
+    (pkgs.runCommand "hidraw-uaccess" {} ''
+      mkdir -p $out/lib/udev/rules.d
+      echo 'KERNEL=="hidraw*", SUBSYSTEM=="hidraw", TAG+="uaccess"' > $out/lib/udev/rules.d/70-hidraw.rules
+    '')
+  ];
 
   boot.tmp.useTmpfs = true; # Use RAM for /tmp
 }
--- hosts/desktop/home/devices.lua
+++ hosts/desktop/home/devices.lua
@@ -1,1 +1,6 @@
 -- no per-device config
+hl.device ({
+  name="ploopy-corporation-ploopy-adept-trackball-mouse",
+  sensitivity = -0.7,
+  scroll_factor = 0.5
+})
